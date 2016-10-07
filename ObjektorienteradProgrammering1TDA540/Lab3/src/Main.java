4e7dc4fc8804c991ec682bd02a79399cf31544a3
Isak <aslundisak@gmail.com>
2016-10-07 19:27:26 +0200

    Added Working solutions for part1 and 2, working on part 3

--- ObjektorienteradProgrammering1TDA540/Lab3/src/Main.java
+++ ObjektorienteradProgrammering1TDA540/Lab3/src/Main.java
@@ -1,24 +1,94 @@
-import java.io.File;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static double[] sine(double freq, double duration) {
-        int n = (int)(duration*SoundDevice.SAMPLING_RATE);
-        double[] a = new double[n];
-        double dx = 2*Math.PI*freq / SoundDevice.SAMPLING_RATE;
-        for (int i = 0; i < n; i = i + 1) {
-            a[i] = Math.sin(i * dx);
-        }
-        return a;
-    }//sine
-    
-    
+    //Pre Uppgift 1
+    /*
     public static void main(String[] args) {
         SoundDevice device = new SoundDevice();
         Song song = new Song(5);
-        song.add(sine(440,2));
-        song.add(sine(880,2));
+        song.add(MusicUtils.sine(440,2));
+        song.add(MusicUtils.sine(880,2));
         song.play(device);
-        song.save(device.getFormat(),new File("twotones.wav"));
-    }//main 
+        song.save(device.getFormat(),new File("sinetest.wav"));
+    }//main
+    */
+
+    //Uppgift 1
+    /*
+    public static void main(String[] args) {
+        SoundDevice device = new SoundDevice();
+        Song song = new Song(5);
+        song.add(MusicUtils.pluck(440,2));
+        song.add(MusicUtils.pluck(880,2));
+        song.play(device);
+        song.save(device.getFormat(),new File("plucktest.wav"));
+    }//main
+    */
+
+    //Uppgift 2
+    /*
+    public static void main(String[] args) {
+        SoundDevice device = new SoundDevice();
+        Song song = new Song(10);
+        song.add(MusicUtils.note(-9, 0.4));
+        song.add(MusicUtils.note(-9, 0.4));
+        song.add(MusicUtils.note(-9, 0.4));
+        song.add(MusicUtils.note(-5, 0.4));
+        song.add(MusicUtils.note(-7, 0.4));
+        song.add(MusicUtils.note(-7, 0.4));
+        song.add(MusicUtils.note(-7, 0.4));
+        song.add(MusicUtils.note(-4, 0.4));
+        song.add(MusicUtils.note(-5, 0.4));
+        song.add(MusicUtils.note(-5, 0.4));
+        song.add(MusicUtils.note(-7, 0.4));
+        song.add(MusicUtils.note(-7, 0.4));
+        song.add(MusicUtils.note(-9, 1));+
+        song.play(device);
+        song.save(device.getFormat(),new File("tonetest.wav"));
+    }//main
+    */
+
+    //Uppgift 3
+    public static void main(String[] args) throws FileNotFoundException {
+
+        SoundDevice device = new SoundDevice();
+        Song song = new Song(20);
+
+        File textSong = new File("elise.txt");
+        String path = textSong.getAbsolutePath();
+
+        System.out.println(path);
+        Scanner sc = new Scanner(path);
+
+        while(sc.hasNext()) {
+            String i = sc.next();
+            System.out.println(i);
+        }
+        sc.close();
+
+        song.play(device);
+        song.save(device.getFormat(),new File("testing"));
+
+        /*
+        song.add(MusicUtils.harmonic(-2, 0.5));
+        song.add(MusicUtils.harmonic(1, 0.5));
+        song.add(MusicUtils.harmonic(3, 0.7));
+
+        song.add(MusicUtils.harmonic(-2, 0.5));
+        song.add(MusicUtils.harmonic(1, 0.5));
+        song.add(MusicUtils.harmonic(4, 0.3));
+        song.add(MusicUtils.harmonic(3, 0.6));
+
+        song.add(MusicUtils.harmonic(-2, 0.5));
+        song.add(MusicUtils.harmonic(1, 0.5));
+        song.add(MusicUtils.harmonic(3, 0.7));
+        song.add(MusicUtils.harmonic(1, 0.5));
+        song.add(MusicUtils.harmonic(-2, 0.7));
+        */
+    }//main
+
+
+
 }//Main
---
+++ ObjektorienteradProgrammering1TDA540/Lab3/src/MusicUtils.java
@@ -0,0 +1,79 @@
+/**
+ * Created by isak on 06/10/16.
+ */
+import java.util.*;
+import java.lang.Math;
+
+public class MusicUtils {
+
+    //before: nothing
+    //after: returns a array with values of a sine wave
+    public static double[] sine(double freq, double duration) {
+        int n = (int)(duration*SoundDevice.SAMPLING_RATE);
+        double[] a = new double[n];
+        double dx = 2*Math.PI*freq / SoundDevice.SAMPLING_RATE;
+        for (int i = 0; i < n; i = i + 1) {
+            a[i] = Math.sin(i * dx);
+        }
+        return a;
+    }//sine
+
+    //before: nothing
+    //after: returns an array with values of a sound wave mimicing plucking a string
+    public static double[] pluck(double freq, double duration){
+
+        //Create the array
+        int n = (int)(duration*SoundDevice.SAMPLING_RATE);
+        double[] a = new double[n];
+
+        //Mutekonstant
+        double K = 0.498;
+
+
+        double p = SoundDevice.SAMPLING_RATE / freq;
+
+        for(int i = 0; i < n; i++){
+            //Fill the p first values with random numbers between [-1, 1]
+            if(i <= p){
+                double rand = new Random().nextDouble() * 2 - 1;
+                a[i] = rand;
+            }else {
+                a[i] = (a[i - (int)p] + a[i - ((int)p - 1)]) * K;
+            }
+        }
+        return a;
+    }//pluck
+
+    //before: assuming pitch is a integer representing a note on the muscial scale ( 0 => A, 3 => C, -4 => F )
+    //after: returns a array with values from a sound wave mimicing plucking a string
+    public static double[] note(int pitch, double duration){
+
+        double freq = 440 * Math.pow(2, ((double)pitch/12));
+        System.out.println(Double.toString(freq));
+
+        return pluck(freq, duration);
+    }//note
+
+
+    //before: takes 2 arrays, both being equal length
+    //after: returns a new array with index values being the mean value of each index value of both arrays
+    public static double[] average(double[] t1, double[] t2){
+        double[] a = new double[t1.length];
+
+        for(int i = 0; i < t1.length; i++){
+            double average = (t1[i] + t2[i]) / 2;
+            a[i] = average;
+        }
+        return a;
+    }//average
+
+    //before: assuming pitch is a integer representing a note on the muscial scale ( 0 => A, 3 => C, -4 => F )
+    public static double[] harmonic(int pitch, double duration){
+        double[] a = average( note(pitch + 12, duration), note(pitch - 12, duration) );
+        double[] harmonic =  average( note(pitch, duration), a);
+
+        return harmonic;
+    }//harmonic
+
+
+}
