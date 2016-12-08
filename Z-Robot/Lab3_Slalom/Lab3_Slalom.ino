6949c8c69fe522bfb5a3341a31ddef5649721968
Isak <aslundisak@gmail.com>
2016-12-08 16:26:03 +0100

    Added working implementation of slalom

--- Z-Robot/Lab3_Slalom/Lab3_Slalom.ino
+++ Z-Robot/Lab3_Slalom/Lab3_Slalom.ino
@@ -7,79 +7,56 @@ Servo pingServo;
 const int pingPin = 13;
 long duration;
 int distance;
-int sumLeft;
-int sumRight;
-
-int sweepArr[180];
 
 void setup() {
  Serial.begin(9600);
  pingServo.attach(12);
  servoLeft.attach(11);
  servoRight.attach(10);
- Serial.println("SETUP STARTED");
- pingServo.write(1);
- delay(1000);
- for(int i = 1; i <= 179; i = i + 2){
-  pingServo.write(i);
-  delay(10);
-  distance = calculateDistance();
-  sweepArr[i] = distance;
- }
- evaluate();
- 
- Serial.print("Left: ");
- Serial.print(sumLeft);
- Serial.print(", Right: ");
- Serial.print(sumRight);
- Serial.println();
- pingServo.write(1);
- delay(1000);
- Serial.println("SETUP COMPLETE");
+
+ pingServo.write(90);
  delay(1000);
 }
 
 void loop() {
- 
- for(int i = 1; i <= 179; i = i + 2){
-  pingServo.write(i);
-  delay(10);
+  forward();
+  delay(75);
   distance = calculateDistance();
-  sweepArr[i] = distance;
-  //printData(i, distance);
- }
- evaluate();
- Serial.print("Left: ");
- Serial.print(sumLeft);
- Serial.print(", Right: ");
- Serial.print(sumRight);
- Serial.println();
- 
- 
-
- for(int i = 179; i >= 1; i = i - 2){
-  pingServo.write(i);
-  delay(10);
-  distance = calculateDistance();
-  sweepArr[i] = distance;
-  //printData(i, distance);
- }
- evaluate();
- Serial.print("Left: ");
- Serial.print(sumLeft);
- Serial.print(", Right: ");
- Serial.print(sumRight);
- Serial.println();
+  Serial.print("Distance Forward: ");
+  Serial.print(distance);
+  Serial.println();
+  
+  if(distance < 6){
+    stopOnSpot();
+    int rightDistance =  rightDist();
+    Serial.print("Right Distance: ");
+    Serial.print(rightDistance);
+    Serial.println();
+    if(rightDistance < 15){
+      turnLeft();
+    }else{
+      turnRight();
+    }
+  }
  
 }
 
-//Prints out current angle and distance to the Srial monitor
-void printData(int deg, int dist){
-  Serial.print(deg);
-  Serial.print("deg : ");
-  Serial.print(dist);
-  Serial.print("cm.");
-  Serial.println();
+int rightDist(){
+  pingServo.write(15);
+  delay(500);
+  int dist = calculateDistance();
+  pingServo.write(90);
+  delay(500);
+  return dist;
+}
+
+void turnLeft(){
+  maneuver(1300, 1300);
+  delay(525);
+}
+void turnRight(){
+  maneuver(1700, 1610);
+  delay(550);
 }
 
 //Returns the distance at the current angle
@@ -99,6 +76,71 @@ int calculateDistance(){
   return distance;
 }
 
+void forward(){
+  servoLeft.writeMicroseconds(1700);
+  servoRight.writeMicroseconds(1390);
+}
+void stopOnSpot(){
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1500);
+}
+
+void maneuver(int speedLeft, int speedRight){
+  servoLeft.writeMicroseconds(speedLeft);
+  servoRight.writeMicroseconds(speedRight);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+void printCompare(){
+  Serial.print("Left: ");
+ Serial.print(sumLeft);
+ Serial.print(", Right: ");
+ Serial.print(sumRight);
+ Serial.println();
+}
+*/
+/*
+ * Maneuver(left, right)
+ * Left faster = 1500+
+ * Right Faster = 1500-
+ */
+
+/*
+void move(){
+  double r = (sumLeft / sumRight);
+  double v;
+  //More space to the left, turn left
+  if(r > 1){
+    v = (1/r) * 200;
+    maneuver(1500 - v, 1300);
+  }else if(r > 4){
+    v = (1/(r/2)) * 200;
+    maneuver(1500 - v, 1300);
+  }
+  //Turn Right
+  else if(r < 1){
+    v = r * 200;
+    maneuver(1700, 1500 + v);
+  } 
+}
+*/
+/*
 void evaluate(){
   sumLeft = 0;
   sumRight = 0;
@@ -111,14 +153,5 @@ void evaluate(){
   }
   
 }
-
-void forward(){
-  servoLeft.writeMicroseconds(1700);
-  servoRight.writeMicroseconds(1390);
-}
-
-void maneuver(int speedLeft, int speedRight){
-  servoLeft.writeMicroseconds(speedLeft);
-  servoRight.writeMicroseconds(speedRight);
-}
+*/
 
